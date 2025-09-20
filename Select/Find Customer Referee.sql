8d047779b645cc2e499488cc2d84026a2e3fdbfa
kalyanitadela <144878555+kalyanitadela@users.noreply.github.com>
2025-09-20 14:53:51 +0530

    Create  Find Customer Referee.sql

---
+++ Select/Find Customer Referee.sql
@@ -0,0 +1,37 @@
+Table: Customer
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+Find the names of the customer that are either:
+1.referred by any customer with id != 2.
+2.not referred by any customer.
+Return the result table in any order.
+MYSQL code:
+
+SELECT name FROM Customer
+WHERE referee_id IS NULL OR  referee_id != 2;
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
